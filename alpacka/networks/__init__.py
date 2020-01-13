131be003d7b73df022e0d81f27ee9020821d2ef0
Piotr Januszewski <piotr.januszewski@pg.edu.pl>
2020-01-13 15:44:29 +0100

    Add TFMetaGraphNetwork

--- alpacka/networks/__init__.py
+++ alpacka/networks/__init__.py
@@ -4,6 +4,7 @@ import gin
 
 from alpacka.networks import core
 from alpacka.networks import keras
+from alpacka.networks import tensorflow
 
 
 # Configure networks in this module to ensure they're accessible via the
@@ -16,3 +17,4 @@ def configure_network(network_class):
 
 DummyNetwork = configure_network(core.DummyNetwork)  # pylint: disable=invalid-name
 KerasNetwork = configure_network(keras.KerasNetwork)  # pylint: disable=invalid-name
+TFMetaGraphNetwork = configure_network(tensorflow.TFMetaGraphNetwork)  # pylint: disable=invalid-name
