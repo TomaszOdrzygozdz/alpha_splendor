2d1e39e24ee52d8d678b582ded1ed88147da1da3
pmilos <pmilos@mimuw.edu.pl>
2020-02-20 11:43:43 +0100

    Revert "AutoBatchStepper"
    This reverts commit f41b6e8c1f4a0206e319738e6fa7152d79f6461d.

--- alpacka/batch_steppers/__init__.py
+++ alpacka/batch_steppers/__init__.py
@@ -4,20 +4,14 @@ import gin
 
 from alpacka.batch_steppers import local
 from alpacka.batch_steppers import ray
-import os
 
 # Configure agents in this module to ensure they're accessible via the
 # alpacka.batch_steppers.* namespace.
-def configure_batch_stapper(batch_stepper_class, name=None):
+def configure_batch_stapper(batch_stepper_class):
     return gin.external_configurable(
-        batch_stepper_class, module='alpacka.batch_steppers', name=name
+        batch_stepper_class, module='alpacka.batch_steppers'
     )
 
-if "LOCAL_RUN" in os.environ:
-    LocalBatchStepper = configure_batch_stapper(local.LocalBatchStepper,
-                                                "AutoBatchStepper")  # pylint: disable=invalid-name
-    RayBatchStepper = configure_batch_stapper(ray.RayBatchStepper)  # pylint: disable=invalid-name
-else:
-    LocalBatchStepper = configure_batch_stapper(local.LocalBatchStepper)  # pylint: disable=invalid-name
-    RayBatchStepper = configure_batch_stapper(ray.RayBatchStepper,
-                                              "AutoBatchStepper")  # pylint: disable=invalid-name
+
+LocalBatchStepper = configure_batch_stapper(local.LocalBatchStepper)  # pylint: disable=invalid-name
+RayBatchStepper = configure_batch_stapper(ray.RayBatchStepper)  # pylint: disable=invalid-name
