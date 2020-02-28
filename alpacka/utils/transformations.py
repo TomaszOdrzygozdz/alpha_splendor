3b148b99a67f49f51f9b3305e611daaa7552e069
konradczechowski <konrad.czechowski@gmail.com>
2020-02-28 11:27:24 +0100

    Docstring for discount_cumsum().

--- alpacka/utils/transformations.py
+++ alpacka/utils/transformations.py
@@ -6,16 +6,23 @@ import scipy.signal
 def discount_cumsum(x, discount):
     """Magic from rllab for computing discounted cumulative sums of vectors.
 
-    input:
-        vector x,
-        [x0,
-         x1,
-         x2]
+    Args:
+        x (np.array): sequence of floats (eg. rewards from a single episode in
+            RL settings)
+        discount (float): discount factor (in RL known as gamma)
 
-    output:
-        [x0 + discount * x1 + discount^2 * x2,
-         x1 + discount * x2,
-         x2]
+    Returns:
+        Array of cumulative discounted sums. For example:
+
+        If vector x has a form
+            [x0,
+             x1,
+             x2]
+
+        Then the output would be:
+            [x0 + discount * x1 + discount^2 * x2,
+             x1 + discount * x2,
+             x2]
     """
     return scipy.signal.lfilter(
         [1], [1, float(-discount)], x[::-1], axis=0
