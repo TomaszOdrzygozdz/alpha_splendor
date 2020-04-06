polyak averaging, marjor bugfix (next_observation) and code cleanup

alpacka/trainers/temporaldifference.py
@@ -1,13 +1,11 @@
 """Temporal difference trainer."""
 
-import math
 import gin
 import numpy as np
 
 from alpacka import data
 from alpacka.trainers import base
 from alpacka.trainers import replay_buffers
-
 
 
 @gin.configurable
@@ -21,10 +19,10 @@ def target_n_return(episode, n, gamma):
 
     cum_rewards = []
     bootstrap_obs = []
-    bootstrap_lambdas = []
+    bootstrap_gammas = []
     for curr_obs_index in reversed(range(ep_len)):
         reward_to_remove = 0 if curr_obs_index + n >= ep_len \
-            else math.pow(gamma, n - 1) * rewards[curr_obs_index + n]
+            else gamma ** (n - 1) * rewards[curr_obs_index + n]
         cum_reward -= reward_to_remove
         cum_reward *= gamma
         cum_reward += rewards[curr_obs_index]
@@ -32,17 +30,17 @@ def target_n_return(episode, n, gamma):
         bootstrap_index = min(curr_obs_index + n, ep_len)
         bootstrap_ob = \
             episode.transition_batch.next_observation[bootstrap_index - 1]
-        bootstrap_lambda = math.pow(gamma, bootstrap_index - curr_obs_index)
+        bootstrap_lambda = gamma ** (bootstrap_index - curr_obs_index)
         if bootstrap_index == ep_len and not truncated:
             bootstrap_lambda = 0
 
         cum_rewards.append(cum_reward)
         bootstrap_obs.append(bootstrap_ob)
-        bootstrap_lambdas.append(bootstrap_lambda)
+        bootstrap_gammas.append(bootstrap_lambda)
 
     return np.array(cum_rewards, dtype=np.float)[::-1, np.newaxis], \
            np.array(bootstrap_obs)[::-1], \
-           np.array(bootstrap_lambdas, dtype=np.float)[::-1, np.newaxis]
+           np.array(bootstrap_gammas, dtype=np.float)[::-1, np.newaxis]
 
 
 class TDTrainer(base.Trainer):
@@ -98,10 +96,10 @@ class TDTrainer(base.Trainer):
             hierarchy_depth=len(replay_buffer_sampling_hierarchy),
         )
         self._sampling_hierarchy = replay_buffer_sampling_hierarchy
-        self.polyak_coeff_ = polyak_coeff
-        if self.polyak_coeff_ is not None:
-            self.target_network_ = network_fn()
-            self.target_network_params_ = None
+        self._polyak_coeff = polyak_coeff
+        if self._polyak_coeff is not None:
+            self._target_network = network_fn()
+            self._target_network_params = None
 
     def add_episode(self, episode):
         buckets = [
@@ -117,29 +115,31 @@ class TDTrainer(base.Trainer):
         )
 
     def train_epoch(self, network):
-        if self.polyak_coeff_ is not None:
+        if self._polyak_coeff is not None:
             current_params = network.params
-            if self.target_network_params_ is None:
-                self.target_network_params_ = current_params
+            if self._target_network_params is None:
+                self._target_network_params = current_params
 
+            # TODO(pm): this works when nn parameters are given by
+            # a list np.arrays, e.g keras nn. Make it work for otherwise
             target_network_params_ = []
             for target_nn_layer, current_nn_layer in \
-                    zip(self.target_network_params_, current_params):
+                    zip(self._target_network_params, current_params):
                 target_network_params_.append(
-                    self.polyak_coeff_ * target_nn_layer +
-                    (1.0 - self.polyak_coeff_) * current_nn_layer)
+                    self._polyak_coeff * target_nn_layer +
+                    (1.0 - self._polyak_coeff) * current_nn_layer)
 
-            self.target_network_params_ = target_network_params_
-            self.target_network_.params = self.target_network_params_
+            self._target_network_params = target_network_params_
+            self._target_network.params = self._target_network_params
         else:
-            self.target_network_ = network
+            self._target_network = network
 
         def data_stream():
             for _ in range(self._n_steps_per_epoch):
-                obs, (cum_reward, bootstrap_obs, bootstrap_lambda) = \
+                obs, (cum_reward, bootstrap_obs, bootstrap_gamma) = \
                     self._replay_buffer.sample(self._batch_size)
-                preds = self.target_network_.predict(bootstrap_obs)
-                target = cum_reward + bootstrap_lambda * preds
+                preds = self._target_network.predict(bootstrap_obs)
+                target = cum_reward + bootstrap_gamma * preds
                 yield obs, target
 
         return network.train(data_stream)